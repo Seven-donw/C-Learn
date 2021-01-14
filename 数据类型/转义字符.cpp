fb16a6e6864dbea93a51e6c0f3d0cb5d9783ea0f
Seven-donw <63043228+Seven-donw@users.noreply.github.com>
2021-01-14 19:03:48 +0800

    Create 转义字符.cpp

---
+++ 数据类型/转义字符.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main2()
+{
+	//转移字符 
+
+	//1、换行符 \n
+	//cout << "Hello World\n" 在c语言中
+	cout << "Hello World" << endl;
+
+	//2、反斜杠 \\
+	
+	cout << "\\" << endl;
+
+	// 3、水平制表符 \t;   对其后面的数据
+	cout << "aaaaaa\thello world" << endl;
+	cout << "aaa\thello world" << endl;
+	cout << "aaaaa\thello world" << endl;
+	system("pause");
+
+	return 0;
+
+}
