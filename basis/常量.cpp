dcd3f664b4af3aea611a9cb97300b1dd2ce93d91
Seven-donw <63043228+Seven-donw@users.noreply.github.com>
2021-01-14 15:31:05 +0800

    Update 常量.cpp

--- basis/常量.cpp
+++ basis/常量.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+using namespace std;
+//#define（宏常量)  #define 常量名 常量值
+#define Day 7 
 
+int main3()
+{
+	cout << "一周有" << Day << "天" << endl;
+	
+	//const （修饰的变量） const 数据类型 常量名 = 常量值
+	const int month = 12;
+
+	cout << "一年有" << month << "个月" << endl;
+
+	system("pause");
+
+	return 0;
+
+}
