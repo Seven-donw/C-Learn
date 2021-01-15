166ad2bfb35c9171440477301228f443f21fdea7
Seven-donw <63043228+Seven-donw@users.noreply.github.com>
2021-01-15 15:09:16 +0800

    Update 算术运算符.cpp

--- 运算符/算术运算符.cpp
+++ 运算符/算术运算符.cpp
@@ -7,6 +7,11 @@ int main() {
 
     cout << "a+b=" << a+b << endl;
     cout << "a-b=" << a-b << endl;
+    cout << "a*b=" << a*b << endl;
+    cout << "a/b=" << a/b << endl; //整数除法输出整数
+    /*
+        0不能作除数 否程序出错
+    */
 
     system("pause")
 
