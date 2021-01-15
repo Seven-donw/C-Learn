b814b933992ab8e8af6724d490b6add0b08f2c60
Seven-donw <63043228+Seven-donw@users.noreply.github.com>
2021-01-15 15:06:19 +0800

    Update 算术运算符.cpp

--- 运算符/算术运算符.cpp
+++ 运算符/算术运算符.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+using namespace std;
 
+int main() {
+    int a = 10;
+    int b = 3;
+
+    cout << "a+b=" << a+b << endl;
+    cout << "a-b=" << a-b << endl;
+
+    system("pause")
+
+    return 0;
+
+}
