34a60e4ba4608c64197e9246269639c52e04256c
Seven-donw <63043228+Seven-donw@users.noreply.github.com>
2021-01-15 17:18:05 +0800

    Update 4.比较运算符.cpp

--- 运算符/4.比较运算符.cpp
+++ 运算符/4.比较运算符.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+using namespace std;
 
+int main() {
+    
+    // 0假 1真
+    // "=="等于
+    int a = 10;
+    int b = 20;
+    cout << (a==b) << endl;
+
+    // "!="不等于
+    cout << (a!=b) << endl;
+
+    // ">"大于
+    cout << (a>b) << endl;
+
+    // "<"小于
+    cout << (a<b) << endl;
+
+    // ">="大于等于
+    cout << (a>=b) << endl;
+    
+    // "<="小于等于
+    cout << (a<=) << endl;
+
+    system("pause");
+
+    return 0;
+
+}
