979ad5d21d191d179719e4591162cb15895e4d21
Seven-donw <63043228+Seven-donw@users.noreply.github.com>
2021-01-15 19:44:51 +0800

    Update 5.逻辑运算符.cpp

--- 运算符/5.逻辑运算符.cpp
+++ 运算符/5.逻辑运算符.cpp
@@ -1,6 +1,11 @@
  #include<iostream>
  using namespace std;
 //用于根据表达式的值返回真值（1）或假值（0）
+/*
+ 运算符："!" 术语：非 结果：如果a为假，则!a为真;如果a为真，则!a为假
+ 运算符："&" 术语：与 结果：如果a和b都为真，则结果为真，否则为假。
+ 
+*/
 int main() {
 
     system("pause");
